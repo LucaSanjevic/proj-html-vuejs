a683d6a9e9f529179ae92b39172661a7dc96b781
LucaSanjevic <luca-sanjevic@hotmail.it>
2021-05-26 03:30:23 +0200

    andando avanti

---
+++ myScript.js
@@ -0,0 +1,73 @@
+var app = new Vue ({
+    el: "#app",
+    data : {
+        listaLinks:[
+            
+            {
+              text: "Shopping Cart",
+              url: "#"
+            },
+
+            {
+                text:"My Account",
+                url: "#"
+            },
+
+            {
+                text:"",
+                url:"#",
+                icon: "fa-user"
+            },
+        ],
+
+        footerData:{
+            twitterData: [
+            {
+                text: "lorem",
+            },
+            {
+                text: "lorem",
+            },
+            {
+                text: "lorem",
+            },
+            {
+                text: "lorem",
+            },
+            {
+                text: "lorem",
+            },
+            {
+                text: "lorem",
+            },
+        ],
+
+        links: [
+            {
+                text: "lorem",
+                link: "#"
+            },
+            {
+                text: "lorem",
+                link: "#"
+            },
+            {
+                text: "lorem",
+                link: "#"
+            },
+            {
+                text: "lorem",
+                link: "#"
+            },
+            {
+                text: "lorem",
+                link: "#"
+            },
+            {
+                text: "lorem",
+                link: "#"
+            },
+        ],
+            }
+        }
+});
